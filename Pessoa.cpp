a3895f61ce58e39e188456ed17e46d3636bb9a73
JoaoZaros <123007506+JoaoZaros@users.noreply.github.com>
2024-09-12 01:37:31 +0000

    classe endereço

--- Pessoa.cpp
+++ Pessoa.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Data.cpp"
+#include "Endereco.cpp"
 using namespace std;
 
 class Pessoa{
@@ -8,10 +9,11 @@ class Pessoa{
         string sobrenome;
         long cpf;
         Data nascimento;
+        Endereco endereco;
 
     public:
-        Pessoa(string nome, string sobrenome, long cpf, int dia, int mes, int ano):
-        nome{nome}, sobrenome{sobrenome}, nascimento{dia, mes, ano} //assim
+        Pessoa(string nome, string sobrenome, long cpf, int dia, int mes, int ano, Endereco endereco):
+        nome{nome}, sobrenome{sobrenome}, nascimento{dia, mes, ano}, endereco{endereco} //assim
         {
             this->cpf = cpf; //ou assim
         }
@@ -47,5 +49,9 @@ class Pessoa{
 
         string getNascimento(){
             return this->nascimento.getData();
+        }
+
+        string getEndereco(){
+            return this->endereco.getEndereco();
         }
 };
--- main.cpp
+++ main.cpp
@@ -3,10 +3,12 @@
 using namespace std;
 
 int main(){
-    Pessoa p ("João", "Zaros", 52839483823, 9, 11, 2024);
+    Endereco e("Rua Java", 105, "Cond Ilha de Bali");
+    Pessoa p("João", "Zaros", 52839483823, 9, 11, 2024, e);
 
     cout <<"Dados da pessoa" << endl;
     cout << "Nome: " << p.getNome() << " " << p.getSobrenome() << endl;
     cout << "CPF: " << p.getCPF() << endl;
     cout << "Data de nascimento: " << p.getNascimento() << endl;
+    cout << "Endereco: " << p.getEndereco() << endl;
 }
