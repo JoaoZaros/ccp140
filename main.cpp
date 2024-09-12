454d865139edb61ab9df9d1bd544613da16a94bf
JoaoZaros <123007506+JoaoZaros@users.noreply.github.com>
2024-09-12 00:49:03 +0000

    Adição da classe pessoa

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include "Pessoa.cpp"
+using namespace std;
 
 int main(){
-    std::cout << "hello world\n";
+    Pessoa p;
+    p.setNome("João");
+    p.setSobrenome("Zaros");
+    p.setCPF(52839483823);
+
+    cout <<"Dados da pessoa" << endl;
+    cout << "Nome: " << p.getNome() << " " << p.getSobrenome() << endl;
+    cout << "CPF: " << p.getCPF() << endl;
 }
