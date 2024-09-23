7b4123c6c409af8d59b8c231640b73f4f90d0023
JoaoZaros <123007506+JoaoZaros@users.noreply.github.com>
2024-09-23 22:23:14 +0000

    Aula faltei

---
+++ Pessoa1.cpp
@@ -0,0 +1,68 @@
+#pragma once
+#include <iostream>
+#include "Data.cpp"
+#include "Endereco.cpp"
+using namespace std;
+
+class Pessoa{
+    private:
+        string nome;
+        string sobrenome;
+        long cpf;
+        Data nascimento;
+        Endereco endereco;
+    
+    public:
+        Pessoa(string nome, string sobrenome, long cpf,
+               int dia, int mes, int ano, Endereco endereco):
+        nome{nome}, sobrenome{sobrenome}, nascimento{dia, mes, ano},
+        endereco{endereco}
+        {
+            this->cpf = cpf;
+        }
+
+        void setNome(string nome, string sobrenome){
+            this->nome = nome;
+            this->sobrenome = sobrenome;
+        }
+
+        void setNome(string nome){
+            this->nome = nome;    
+        }
+
+        void setSobrenome(string sobrenome){
+            this->sobrenome = sobrenome;
+        }
+
+        void setCPF(long cpf){
+            this->cpf = cpf;
+        }
+
+        string getNome(){
+            return this->nome;
+        } 
+
+        string getSobrenome(){
+            return sobrenome;
+        }
+
+        long getCPF(){
+            return cpf;
+        }
+
+        string getNascimento(){
+            return this->nascimento.getData();
+        }
+
+        string getEndereco(){
+            return this->endereco.getEndereco();
+        }
+
+        virtual void print(){
+            cout << "Dados da pessoa" << endl;
+            cout << "Nome: " << this->getNome() << " " << this->getSobrenome() << endl;
+            cout << "CPF: " << this->getCPF() << endl;
+            cout << "Data de nascimento: " << this->getNascimento() << endl;
+            cout << "Endereco: " << this->getEndereco() << endl;
+        }
+};        
---
+++ main1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include "Pessoa1.cpp"
+#include "Funcionario.cpp"
+using namespace std;
+
+int main(){
+    Endereco e("Rua R", 1, "Bairro B");
+    Pessoa p("Pessoa", "Sobrenome",123456789, 9, 11, 2024, e);
+    Funcionario f("Funcionario", "Sobrenome", 987654321, 18, 9, 2024, e, 10000, 1);
+
+    Pessoa* pessoas[2];
+    pessoas[0] = &p;
+    pessoas[1] = &f;
+
+    for(int i=0;i<2;i++){
+        pessoas[i]->print();
+    }
+}
